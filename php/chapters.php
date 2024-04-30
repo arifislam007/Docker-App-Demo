b5d7b29e2887a2a48a4667a26ea3bec93e12675c
MD. AREFUL ISLAM <32135229+arifislam007@users.noreply.github.com>
2024-04-30 18:58:11 +0600

    Create chapters.php

---
+++ php/chapters.php
@@ -0,0 +1,45 @@
+<?php
+$pageTitle = 'List of Chapters - Mein Kampf: A History of Dictator';
+include('header.php');
+?>
+
+<h2>List of Chapters</h2>
+
+<h3>Volume 1: A Reckoning</h3>
+<ul>
+    <li>In the House of My Parents</li>
+    <li>Years of Study and Suffering in Vienna</li>
+    <li>General Political Considerations Based on My Vienna Period</li>
+    <li>Munich</li>
+    <li>The World War</li>
+    <li>War Propaganda</li>
+    <li>The Revolution</li>
+    <li>The Beginning of My Political Activity</li>
+    <li>The German Workers' Party</li>
+    <li>Causes of the Collapse</li>
+    <li>Nation and Race</li>
+    <li>The First Period of Development of the National Socialist German Workers' Party</li>
+</ul>
+
+<h3>Volume 2: The National Socialist Movement</h3>
+<ul>
+    <li>Philosophy and Party</li>
+    <li>The State</li>
+    <li>Subjects and Citizens</li>
+    <li>Personality and the Conception of the Völkisch State</li>
+    <li>Philosophy and Organization</li>
+    <li>The Struggle of the Early Period – The Significance of the Spoken Word</li>
+    <li>The Struggle with the Red Front</li>
+    <li>The Strong Man is Mightiest Alone</li>
+    <li>Basic Ideas Regarding the Meaning and Organization of the SA</li>
+    <li>Federalism as a Mask</li>
+    <li>Propaganda and Organization</li>
+    <li>The Trade-Union Question</li>
+    <li>German Alliance Policy After the War</li>
+    <li>Eastern Orientation or Eastern Policy</li>
+    <li>The Right of Emergency Defense</li>
+</ul>
+
+<?php
+include('footer.php');
+?>
