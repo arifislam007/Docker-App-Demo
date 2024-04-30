900a6e14604aa6492a50b75ccddb1d2656c48e82
MD. AREFUL ISLAM <32135229+arifislam007@users.noreply.github.com>
2024-04-30 18:56:36 +0600

    Create about.php

---
+++ php/code/about.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = 'About the Book - Mein Kampf: A History of Dictator';
+include('header.php');
+?>
+
+<h2>About the Book</h2>
+<p><i>Mein Kampf</i> ("My Struggle") is a book written by Adolf Hitler, combining elements of autobiography with an exposition of Hitler's political ideology. Penned during his imprisonment following the failed Munich Putsch of 1923, the book outlines Hitler's plans for transforming German society into one based on race. First published in 1925 as Volume 1 and followed by a second Volume in 1926, <i>Mein Kampf</i> provides extensive insight into the early ideological foundations of what would later become Nazi Germany.</p>
+
+<p>The book is divided into two volumes. The first, titled "A Reckoning," elaborates on Hitler’s worldview, including his youth, early aspirations, and the political climate of Germany post-World War I which he believed was corrupted by the Weimar Republic and influenced by Jews. The second volume, "The National Socialist Movement," describes the early years of the Nazi Party and his goals for its future, emphasizing the need for a national revival based on principles of race and space.</p>
+
+<p>Throughout the book, Hitler advocates for a forceful reclamation of German territory, the unification of all Germans, a strong hatred toward Jews and Communism, and the importance of obedience and hierarchy in ensuring the success of the Aryan race. His work is filled with anti-Semitic rhetoric and discussions on the strategies of propaganda and mass mobilization.</p>
+
+<p><i>Mein Kampf</i> has been a controversial book since its first release and remains heavily critiqued and studied for its contents and impact on Nazi ideology. The work is often analyzed in historical and political studies to understand the depths of Nazi beliefs and the foundation for the Holocaust.</p>
+
+<?php
+include('footer.php');
+?>
